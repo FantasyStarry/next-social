5e7f6dc2380a8dc869ce01ea5781e26897a3ac20
FantasyStarry <spongzi@outlook.com>
2024-08-29 15:06:58 +0800

    添加圆角

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
   return (
-    <div className="p-4 bg-white shadow-md text-sm">
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm">
       {/* TOP */}
       <div className="flex items-center justify-between text-gray-500 font-medium">
         <span>Sponsored Ads</span>
