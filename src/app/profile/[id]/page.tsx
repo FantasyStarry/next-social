91be0ca9d2e5fd4368bf7e746e4ff1f8a627fcf9
FantasyStarry <spongzi@outlook.com>
2024-09-02 16:02:47 +0800

    连接数据库并且创建数据库表

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,16 +1,49 @@
 import Feed from "@/components/Feed";
 import LeftMenu from "@/components/LeftMenu";
 import RightMenu from "@/components/RightMenu";
+import Image from "next/image";
 import React from "react";
 
 const ProfielPage = () => {
   return (
-    <div className="flex gap-6">
+    <div className="flex gap-6 pt-6">
       <div className="hidden xl:block xl:w-[20%]">
         <LeftMenu type="profile" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://haowallpaper.com/link/common/file/getCroppingImg/15386240242061632"
+                alt=""
+                fill
+                className="object-cover"
+              />
+              <Image
+                src="https://haowallpaper.com/link/common/file/getCroppingImg/9217d5c6d87ce0e8ec7422ca7469db95"
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Elva Weaver</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">123</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">1.23K</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">12.3K</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
